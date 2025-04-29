9aa05c67ee6a9502737ef9cf509501483031c190
zhangxs <1258545759@qq.com>
2025-04-29 15:34:25 +0800

    first commit

---
+++ src/com/hot100/leetcode/code1.java
@@ -0,0 +1,49 @@
+/**
+ * 1. 两数之和
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+ * 你可以按任意顺序返回答案。
+ *
+ * 示例 1：
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ *
+ * 提示：
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * 只会存在一个有效答案
+ */
+
+package com.hot100.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class code1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i]))
+            {
+                return new int[]{map.get(target - nums[i]),i};
+            }
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("no match sum");
+    }
+    public static void main(String[] args) {
+        code1 s = new code1();
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(s.twoSum(nums,target)));
+    }
+}
